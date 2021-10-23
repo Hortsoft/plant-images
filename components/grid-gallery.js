50cc86195459e9ce696c21cde1ec645b8a446ac2
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-10-23 17:45:18 +1300

    jj

--- components/grid-gallery.js
+++ components/grid-gallery.js
@@ -1,13 +1,17 @@
-import React from "react"; 
-
+import React from "react";
+import Image from "next/image";
 export default function GridGallery({ images }) {
   return (
     <div className="grid grid-cols-3 gap-4">
       {images &&
         images.map((imageUrl) => (
-          <img
+          <Image
             src={imageUrl}
-             
+            alt="Picture of something nice"
+            width={16}
+            height={9}
+            layout="responsive"
+            quality={65}
           />
         ))}
     </div>
