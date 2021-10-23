831c19b25ac5a0a8cd74655d46789b7fe3345928
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-10-23 23:15:33 +1300

    dd

--- components/grid-gallery.js
+++ components/grid-gallery.js
@@ -1,20 +1,20 @@
-import Image from "next/image";
+// import Image from "next/image";
 
-export default function GridGallery({ images }) {
-  return (
-    <div className="grid grid-cols-3 gap-4">
-      {images &&
-        images.map((imageUrl) => (
-          <Image
-            key={imageUrl}
-            src={imageUrl}
-            alt="Picture of something nice"
-            width={16}
-            height={9}
-            layout="responsive"
-            quality={65}
-          />
-        ))}
-    </div>
-  );
-}
+// export default function GridGallery({ images }) {
+//   return (
+//     <div className="grid grid-cols-3 gap-4">
+//       {images &&
+//         images.map((imageUrl) => (
+//           <Image
+//             key={imageUrl}
+//             src={imageUrl}
+//             alt="Picture of something nice"
+//             width={16}
+//             height={9}
+//             layout="responsive"
+//             quality={65}
+//           />
+//         ))}
+//     </div>
+//   );
+// }
