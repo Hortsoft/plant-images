e948f1b0a2f20c81d3bd1f110e28a51c786971bf
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-10-23 23:11:49 +1300

    lll

--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,7 @@ import Image from "next/image";
 import GridGallery from "../components/grid-gallery";
 
 export default function Home() {
-  const images = [
-    "/images/bird_1.jpg",
-    "/images/bird_2.jpg",
-    "/images/bird_3.jpg",
-    "/images/bird_4.jpg",
-    "/images/bird_5.jpg",
-    "/images/bird_6.jpg",
-    "/images/bird_7.jpg",
-    "/images/bird_8.jpg",
-    "/images/bird_9.jpg",
-    "/images/bird_10.jpg",
-    "/images/bird_11.jpg",
-    "/images/bird_12.jpg",
-    "/images/bird_13.jpg",
-    "/images/bird_14.jpg",
-    "/images/bird_15.jpg",
-    "/images/bird_16.jpg",
-    "/images/bird_17.jpg",
-    "/images/bird_18.jpg",
-    "/images/bird_19.jpg",
-    "/images/bird_20.jpg",
-  ];
+
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
