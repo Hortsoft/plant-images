426e807fdf2c49749cbca3c34e5fe682c8e6862e
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-10-23 23:14:02 +1300

    jj

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <GridGallery images={images} />
+        {/* <GridGallery images={images} /> */}
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
