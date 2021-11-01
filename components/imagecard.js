405cb192336f71a8e5e35e373c107fd82c4bb731
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-11-02 00:46:30 +1300

    reduced gap between images

--- components/imagecard.js
+++ components/imagecard.js
@@ -4,7 +4,7 @@ const ImageCard = ({ image }) => {
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
           <img src={image.url} alt="" className="w-full" />
-          <div className="px-6 py-4">
+          <div className="px-2 py-2">
             <div className="font-bold text-purple-500 text-xl mb-2">
               {image.name}
             </div>
