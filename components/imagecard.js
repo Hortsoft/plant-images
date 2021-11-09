e8f941e6cd5ad5012537a90649a4b471e0db501f
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-11-10 00:19:39 +1300

    Removed title and minmised gap to px py2

--- components/imagecard.js
+++ components/imagecard.js
@@ -1,18 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const ImageCard = ({ image }) => {
-    return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg">
-          <img src={image.url} alt="" className="w-full" />
-          <div className="px-2 py-2">
-            <div className="font-bold text-purple-500 text-xl mb-2">
-              {image.name}
-            </div>
-             
-          </div>
-         
-        </div>
-    );
-}
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg px-2 py-2">
+      <img src={image.url} alt={image.name} className="w-full" />
+    </div>
+  );
+};
 
 export default ImageCard;
