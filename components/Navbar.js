ba59a3edd75b49d62884c24583136fcf49553073
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-11-13 22:59:47 +1300

    Walks Category Added

--- components/Navbar.js
+++ components/Navbar.js
@@ -146,7 +146,7 @@ function Nav() {
                   href="#"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  Reports
+                  Walks
                 </a>
               </div>
             </div>
