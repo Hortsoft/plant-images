4ad70e48ba0afee463b0cb3b45bebfd134d59035
Hortsoft <71443445+Hortsoft@users.noreply.github.com>
2021-11-17 00:03:14 +1300

    jj

--- components/imagecard.js
+++ components/imagecard.js
@@ -3,7 +3,9 @@ import React from "react";
 const ImageCard = ({ image }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg px-2 py-2">
+    <a href={image.url}>
       <img src={image.url} alt={image.name} className="w-full" />
+      </a>
     </div>
   );
 };
